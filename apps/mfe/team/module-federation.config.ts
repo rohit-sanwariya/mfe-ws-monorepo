84e5ce2f2ec938cbb6a5d4c5387193cb766c980d
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2024-09-02 14:15:59 +0530

    team remote  added

--- apps/mfe/team/module-federation.config.ts
+++ apps/mfe/team/module-federation.config.ts
@@ -3,7 +3,7 @@ import { ModuleFederationConfig } from '@nx/webpack';
 const config: ModuleFederationConfig = {
   name: 'team',
   exposes: {
-    './Module': 'app/mfe/team/src/app/remote-entry/entry.module.ts',
+    './Module': 'apps/mfe/team/src/app/remote-entry/entry.module.ts',
   },
 };
 
